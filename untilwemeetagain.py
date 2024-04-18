c477a0395c3cd826f62f9d8018eba44a927a148c
Sikanotan <131864715+Sikanotan@users.noreply.github.com>
2024-04-18 10:55:06 +0200

    correct bugs due to misplaced control

--- untilwemeetagain.py
+++ untilwemeetagain.py
@@ -1,5 +1,6 @@
 import os
 import argparse
+import smtplib
 
 cwd_directory = os.path.dirname(os.path.realpath(__file__))
 os.chdir(cwd_directory)
@@ -11,43 +12,11 @@ parser = argparse.ArgumentParser(prog="Is my address mail valid ?",
 
 parser.add_argument('address_email', type=str, metavar="address",nargs='+', default='None')
 
-if __name__ == "__main__":
-
-	"""
-		The purpose of this script is to extract mail address and test with an
-		STMP validation if the email seems to exist.
-	"""
-
-	args = parser.parse_args()
-
-	if args != None:
-
-		if len(args) == 1:
-
-			if isanemail(address):
-				print("The email address is valid.")
-				print("Trying to reaching server...")
-				doesItExist, message = isemailexist(address)
-
-				if doesItExist:
-					print("Email address is valid.")
-				else:
-					print(message)
-
-			else:
-				print("Control the given address")
-
-		else:
-			print("Too many address were given.")
-
-	else:
-		print("No address mail was given.")
-
 def isemailexist (address):
-	"""
-		Try to connect with the server and to contact the endpoint.
-	"""
-	domain = address.split('@')[1]
+    """
+        Try to connect with the server and to contact the endpoint.
+    """
+    domain = address.split('@')[1]
 
     try:
   
@@ -74,70 +43,106 @@ def isemailexist (address):
         return False, f"An error occurred: {e}"
 
 
-def isanemail(address): -> bool
-	"""
-		Check if the given string is an email following RFC 822.
-		https://www.w3.org/Protocols/rfc822/
+def isanemail(address)->bool:
+    """
+        Check if the given string is an email following RFC 822.
+        https://www.w3.org/Protocols/rfc822/
 
-		local-part@domain
+        local-part@domain
 
-		Could be done with a regex
-	"""
-	# The special char are unauthorized execept .
-	address_validity = False
-	error_message = ""
-	specials   = ["(", ")", "<", ">", "@", ",", ";", ":", "\\", "<", ">", ".",\
-	"[","]", "\""]
-	# And the range bellow 38
-	ascii_unauthorized = [127, 177]
+        Could be done with a regex
+    """
+    # The special char are unauthorized execept .
+    address_validity = False
+    error_message = ""
+    specials   = ["(", ")", "<", ">", "@", ",", ";", ":", "\\", "<", ">", ".",\
+    "[","]", "\""]
+    # And the range bellow 38
+    ascii_unauthorized = [127, 177]
 
-	addr_spec = str(address).split("@")
+    addr_spec = str(address).split("@")
 
-	if addr_spec == 2:
-		
-		local_part, domain = addr_spec
+    if len(addr_spec) == 2:
+        
+        local_part, domain = addr_spec
 
-		mail = local_part + domain
-		dot_found = False
+        mail = local_part + domain
+        dot_found = False
+        if "." in domain:
 
-		for each in mail:
+            address_validity = True
+        for each in mail:
+            if each in specials:
 
-			if each in specials:
+                if each == ".":
+                    if dot_found == True:
+                        error_message = "The given address is not valid."
+                        address_validity = False
+                        break
+                    else:
+                        dot_found = True
+                else:
+                    error_message = "The given address is not valid."
+                    address_validity = False
+                    break
+            else:
+                dot_found = False
 
-				if each == ".":
-					if dot_found == True:
-						error_message = "The given address is not valid."
-						break
-					else:
-						dot_found = True
-				else:
-					error_message = "The given address is not valid."
-					break
-			else:
-				dot_found = False
+            ascii_value = ord(each)
 
-			ascii_value = ord(each)
+            if ascii_value in ascii_unauthorized:
 
-			if ascii_value in ascii_unauthorized:
+                error_message = "The given address is not valid."
+                address_validity = False
+                break
+            elif ascii_value < 38:
+                error_message = "The given address is not valid."
+                address_validity = False
+                break
+            else:
+                char_valid = True
 
-				error_message = "The given address is not valid."
-				break
-			elif ascii_value < 38:
-				error_message = "The given address is not valid."
-				break
-			else:
-				char_valid = True
+        
+        else:
+            error_message = "The given address is not valid."
 
-		if "." in domain:
+    else:
 
-			address_validity = True
-		else:
-			error_message = "The given address is not valid."
+        error_message = "The given address is not valid."
 
-	else:
+    print(error_message)
 
-		error_message = "The given address is not valid."
+    return address_validity
 
-	print(error_message)
+if __name__ == "__main__":
 
-	return address_validity
+    """
+        The purpose of this script is to extract mail address and test with an
+        STMP validation if the email seems to exist.
+    """
+
+    args = parser.parse_args()
+
+    if args != None:
+
+        if len(args.address_email) == 1:
+            address = args.address_email[0]
+            if isanemail(address):
+                print("The email address is valid.")
+                print("Trying to reaching server...")
+                doesItExist, message = isemailexist(address)
+
+                if doesItExist:
+                    print("Email address is valid.")
+                else:
+                    print(message)
+
+            else:
+                print("Control the given address")
+
+        else:
+            print("Too many address were given.")
+
+    else:
+        print("No address mail was given.")
+
